3fcf6f5e30fb65da4148a60ea076e5042d0a27f1
Bhushanpatil6898 <148213732+Bhushanpatil6898@users.noreply.github.com>
2024-09-30 17:34:31 +0530

    add

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
  
   addProduct,
-  Genrate,
+
   GetProduct,
   Login,
   Profile,
@@ -17,7 +17,7 @@ routes.post('/login', Login);
 routes.post('/add-product', addProduct);
 routes.get('/profile', Profile);
 routes.get('/get-product', GetProduct);
-routes.post('/get-response',Genrate );
+
 
 
 export default routes;
