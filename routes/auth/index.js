5c33d8a3cdc096ed6031b30b146e1af9646b0277
Mr. Bhushan Gulabrao Patil. <143311716+Bhushan6898@users.noreply.github.com>
2024-12-23 17:22:59 +0530

    Update index.js

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -8,14 +8,14 @@ import {
   getAllLogs
  
 } from "../../services/auth/index.js"
-
+import { cheack } from "../../middleware/jwt/index.js";
 
 const routes = express.Router();
 routes.get('/ping', server);
 routes.post('/verification', verification);
 routes.post('/create-otp', genrateotp);
 routes.get('/get-client', getAllUsers);
-routes.get('/get-notification', getNotification);
+routes.get('/get-notification',cheack, getNotification);
 routes.get('/get-logs', getAllLogs);
 
 
